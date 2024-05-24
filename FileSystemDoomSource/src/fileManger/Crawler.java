12f35d18b0ac696fb251af1e04a3726eed7becef
LABMATT <matt42764@gmail.com>
2024-05-25 03:09:13 +1000

    Modifed Crawl

--- FileSystemDoomSource/src/fileManger/Crawler.java
+++ FileSystemDoomSource/src/fileManger/Crawler.java
@@ -73,4 +73,64 @@ public class Crawler {
             failedIndexs.add(file);
         }
     }
+
+
+
+    public IndexObject crawlDIR(String rootlocation) {
+
+        List<File> auxiliaryCrawl = new ArrayList<>();
+        List<File> indexedFiles = new ArrayList<>();
+        List<File> failedIndexs = new ArrayList<>();
+
+        boolean validPath = true;
+
+        Path path = Paths.get(rootlocation);
+        if (!Files.exists(path)) {
+
+            validPath = false;
+        }
+
+
+        if (validPath) {
+
+
+            // Preform inital crawl starting from root Directory. If a files found then add to index list, else add to next round.
+            File file = new File(rootlocation);
+            crawlSubDirs(file, indexedFiles, auxiliaryCrawl, failedIndexs);
+
+            // Loop though all index files until the list is empty. Crawl each file.
+            while (!auxiliaryCrawl.isEmpty()) {
+
+                crawlSubDirs(auxiliaryCrawl.get(0), indexedFiles, auxiliaryCrawl, failedIndexs);
+                auxiliaryCrawl.remove(0);
+            }
+
+            // Create an Object and return it. Oject contains all the info from this index.
+            return new IndexObject(rootlocation, indexedFiles, failedIndexs);
+        }
+
+        // if an error occured then retrun null.
+        return null;
+    }
+    // Crawl a whole folder adding the files to the indexed file list if found else add to second crawl list.
+    private void crawlSubDirs(File file, List<File> indexedFiles, List<File> auxiliaryCrawl, List<File> failedIndexs) {
+
+        File[] fileList = file.listFiles();
+
+        if (fileList != null) {
+            for (File scannedItem : fileList) {
+
+                if (scannedItem.isDirectory()) {
+
+
+                    auxiliaryCrawl.add(scannedItem);
+                } else {
+
+                    indexedFiles.add(scannedItem);
+                }
+            }
+        } else {
+            failedIndexs.add(file);
+        }
+    }
 }
